f6e852ed5e4055a11d9344972e843f9fc26ce429
Marques Ivory <Maivory12@icloud.com>
2023-02-27 18:45:26 -0500

    Added table for manager and departments

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,24 @@ DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 USE employees;
 
+DROP TABLE IF EXISTS manager;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE manager (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
